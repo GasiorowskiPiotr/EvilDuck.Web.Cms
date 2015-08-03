cbc1f3b479f546f09c6ed57036a448cb87bfdbfb
unknown <B4P@LW5B4PCEM.dsone.3ds.com>
2015-08-03 23:09:16 +0200

    Security initial data

--- Platform.Core/SecurityMigrations/Configuration.cs
+++ Platform.Core/SecurityMigrations/Configuration.cs
@@ -1,3 +1,8 @@
+using EvilDuck.Platform.Core.Security;
+using EvilDuck.Platform.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 namespace EvilDuck.Platform.Core.SecurityMigrations
 {
     using System;
@@ -16,6 +21,25 @@ namespace EvilDuck.Platform.Core.SecurityMigrations
         protected override void Seed(EvilDuck.Platform.Core.Security.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
+
+            var roleExists = context.Roles.Any(r => r.Name == "Administrator");
+            if (!roleExists)
+            {
+                new RoleManager<IdentityRole, string>(new RolesStore(context)).Create(new IdentityRole("Administrator"));
+            }
+
+            var adminExists = context.Users.Any(u => u.Email == "admin@evilduck.org");
+            if (!adminExists)
+            {
+                var appUser = new ApplicationUser()
+                {
+                    Email = "admin@evilduck.org",
+                    UserName = "admin@evilduck.org"
+                };
+
+                new UsersManager(new UsersStore(context)).Create(appUser, "!QAZxsw2#");
+                new UsersManager(new UsersStore(context)).AddToRole(appUser.Id, "Administrator");
+            }
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
